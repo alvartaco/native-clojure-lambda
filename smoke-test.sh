9f0694127628bfad56651d1c9fdc597204887737
alejandro <alejandro@rithm.nl>
2024-10-29 09:46:46 -0300

    Ports changes to test all VMs at the same time

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+curl -XPOST "http://localhost:9001/2015-03-31/functions/function/invocations" -d '{}'
+curl -XPOST "http://localhost:9002/2015-03-31/functions/function/invocations" -d '{}'
+curl -XPOST "http://localhost:9003/2015-03-31/functions/function/invocations" -d '{}'
